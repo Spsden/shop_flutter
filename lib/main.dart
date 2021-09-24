70b0bbb54c52a9edd92743e3d172e8c49e886181
Spsden <pratapsinghsuraj420@gmail.com>
2021-09-24 07:53:28 -0700

    second day finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalogue/homepage.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Material(
-      child: Center(
-          child: Container(
-        child: Text("Hello Sheetal"),
-      )),
-    ));
+      home: Homepage(),
+    );
   }
 }
